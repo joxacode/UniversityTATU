dbc0db727487ea9251e3a3837330328cdcbde487
joxacode <joxacode@gmail.com>
2023-11-29 19:02:41 +0500

    sqlite

--- University/jazzmin_conf.py
+++ University/jazzmin_conf.py
@@ -1,12 +1,12 @@
 JAZZMIN_SETTINGS = {
     # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Burafshon TTJ",
+    "site_title": "Nurafshon TTJ",
 
     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Burafshon TTJ",
+    "site_header": "Nurafshon TTJ",
 
     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Burafshon TTJ",
+    "site_brand": "Nurafshon TTJ",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
     "site_logo": None,
